61b314741f908d17f3b78aa6bbeeea0133de59a3
Prattay <pdey30@stuy.edu>
2022-05-24 14:06:23 -0400

    upd

---
+++ Customer.java
@@ -0,0 +1,15 @@
+public class Customer{
+  //instance variables
+  private final String name;
+  private TableQueue table;
+  private OrderQueue order;
+
+  public Customer(String name, TableQueue table, OrderQueue order){
+    this.name = name;
+    this.table = table;
+    this.order = order;
+  }
+
+  /* ------------ ACCESSOR METHODS ------------- */
+  
+}
