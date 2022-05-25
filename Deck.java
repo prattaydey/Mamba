933269fbd3764a912d9295c972f57df9499efea7
Prattay Dey <73182203+prattaydey@users.noreply.github.com>
2022-05-25 13:38:33 -0400

    Update Deck.java

---
+++ Deck.java
@@ -0,0 +1,18 @@
+/*
+108 cards, 4 colors
+Each color: one card for 0 / two cards each for 1-9 / two DrawTwo cards / two Skips / two Reverse
+Four Wild cards and Four WildFour cards in entire Deck
+*/
+
+public class Deck{
+  private Card[] cards;
+  private int cardsInDeck;
+
+  public Deck(){
+    cards = new Cards[108];
+  }
+
+  public void reset(){
+    
+  }
+}
