8f9fb0fe7c0050bdd93e0305ab55ee33f0eb593b
unknown <nlabib20@stuy.edu>
2022-05-26 22:45:30 -0400

    v0

---
+++ v0/Card.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class Card{
+
+  private String value;
+  private String color;
+
+  private final String[] allValues =
+  { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "DrawTwo", "Skip", "Reverse", "Wild", "WildFour" };
+
+  private final String[] allColors = { "Red", "Green", "Blue", "Yellow", "Wild" };
+
+  public Card(int number, String color){
+    this.value = allValues[number];
+    this.color = color;
+  }
+
+  public void setValue(int index){
+    value = allValues[index];
+  }
+
+  public void setColor(int index){
+    color = allColors[index];
+  }
+
+  public String getValue(){ return value; }
+
+  public String getColor(){ return color; }
+
+  public boolean isValid(Card previous){
+    return this.getValue().equals( previous.getValue() )
+        || this.getColor().equals( previous.getColor() )
+        || this.getColor().equals( "Wild" )
+        || this.getColor().equals( "WildFour" );
+  }
+
+}
