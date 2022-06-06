2f5a78ebb271a861f56f45848c7844f2a8303f51
Prattay <Prattay@Prashuns-iMac.local>
2022-06-06 00:00:19 -0400

    upd

--- Woo/Woo.java
+++ Woo/Woo.java
@@ -13,7 +13,7 @@ import java.util.Iterator;
 public class Woo{
   private Deck deck;
   private int numBots;
-  private ArrayList totalParticipants;
+  private ArrayList<Participant> totalParticipants;
   private Participant[] totalBots;
   private Participant user;
   private Iterator<Participant> it;
@@ -24,8 +24,8 @@ public class Woo{
 
   public Woo(){
     deck = new Deck();
-    numBots = 0;
-    totalParticipants = new ArrayList<Participant>();
+    numBots = 1;
+    totalParticipants = new ArrayList<>();
     totalBots = new Bot[0];
     user = new Player();
 
@@ -37,44 +37,45 @@ public class Woo{
   }
 
   public void welcome(){
-    System.out.println("Welcome to Team Mamba's UNO card game. \n How many bots do you want to play with? (1-3)");
+    System.out.println("Welcome to Team Mamba's UNO card game. \nHow many bots do you want to play with? (1-3)");
     try {
       numBots = Integer.parseInt(in.readLine());
     }
     catch ( IOException e ) { }
     if (numBots > 0 && numBots < 4){
-      totalBots = new Bot[numBots];
+      System.out.println("Now playing with " + numBots + " bots.");
     }
     else{
       System.out.println("Invalid number of bots! Automatically playing against one bot.");
-      totalBots = new Bot[1];
     }
 
     // adds all the participants, including player and her bots
     totalParticipants.add( user );
-    for (int i = 0; i < totalBots.length; i++){
-      totalParticipants.add( totalBots[i] );
+    for (int i = 0; i < numBots; i++){
+      totalParticipants.add(new Bot());
     }
 
     // sets up the hand of each participant at the start of a game
-    while ( it.hasNext() ){
-      Participant participant = it.next();
-      participant.setup( deck );
-    }
+    // while ( it.hasNext() ){
+    //   Participant p = it.next();
+    //   p.setup( deck );
+    // }
+
+    for (Participant p : totalParticipants) p.setup(deck);
   }
 
-  public void turns(){
-    while ( it.hasNext() ){
+  public void playTurns(){
+    for (Participant participant : totalParticipants) {
       // checks to see if next participant is either a Player or a bot
-      Participant participant = it.next();
+      // Participant participant = it.next();
       if ( participant instanceof Bot ){
-        participant = (Bot) participant;
-        participant.select( deck );
+        Bot bot = (Bot) participant;
+        bot.select( deck );
       }
       else{
-        participant = (Player) participant;
+        user = (Player) participant;
         System.out.println("Your hand is " + participant);
-        System.out.println("Choose what card you would like to select (1 - " + participant.size() + 1 + ") " + "OR draw from the deck" );
+        System.out.println("Choose what card you would like to select (1 - " + (participant.size() ) + ") " + "OR draw from the deck" );
       }
 
     }
@@ -82,6 +83,8 @@ public class Woo{
 
 
   public static void main(String[] args){
-
+    Woo testGame = new Woo();
+    testGame.welcome();
+    testGame.playTurns();
   }
 }
